eea8edd467bb7f1d0fc9a2445151a77d946123ee
Otto <justigo86@gmail.com>
2024-06-20 22:47:52 -0400

    shifted the metadata information from the public directory to the app directory - removed the unnecessary tags from app/layout such as meta, link, title, and head

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,10 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "React App",
+  description: "Web site created with Next.js.",
+};
+
 export default function RootLayout({
   children,
 }: {
@@ -5,21 +12,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <meta name="theme-color" content="#000000" />
-        <meta
-          name="description"
-          content="Web site created using create-react-app"
-        />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
-        <title>React App</title>
-      </head>
       <body>
-        <noscript>You need to enable JavaScript to run this app.</noscript>
+        {/* <noscript>You need to enable JavaScript to run this app.</noscript> */}
         <div id="root">{children}</div>
       </body>
     </html>
