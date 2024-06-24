a0ea76498e88fd95ea5dc24f45ea5475e6b12e32
Otto <justigo86@gmail.com>
2024-06-23 22:50:13 -0400

    continued to work with styling, this time with the hero section - still need to figure out the coffee.jpg background image before finishing the section

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -5,6 +5,7 @@ import { FaGithub, FaLinkedin, FaEnvelope, FaRegFileAlt } from "react-icons/fa";
 import resume from "../../../public/Justin_Otto_Resume.pdf";
 import "./hero.scss";
 import Typed from "typed.js";
+// import image1 from "../../../public/coffee1.jpg";
 
 const intros = [
   "am committed to improving every day.",
@@ -41,82 +42,88 @@ const Hero = (): ReactElement => {
   });
 
   return (
-    <div id="home" className="hero-container">
-      <div className="hero-background-img" />
-      <div className="intro-container">
-        <h1>J. Otto</h1>
-        <p>Web Developer</p>
-        <p className="hero-descriptions">
+    <div
+      id="home"
+      className="h-[110vh] text-left text-white text-3xl overflow-hidden relative bg-transparent mt-36 text-shadow-md"
+    >
+      <div className="absolute bg-hero bg-no-repeat bg-cover bg-center w-full h-full grayscale brightness-[0.45]"></div>
+      <div className="relative top-88 left-28 font-extrabold animate-[heroLoad_0.7s]">
+        <h1 className="inline-block text-8xl mb-2">J. Otto</h1>
+        <p className="mb-2">Web Developer</p>
+        <p className="mb-2">
           and I&nbsp;
-          <span className="typed" ref={el}></span>
+          <span className="font-bold" ref={el}></span>
         </p>
-        <div className="hero-links">
-          <Link
-            className="hero-link"
-            to="projects"
-            smooth={true}
-            duration={1000}
-          >
-            <a className="hero-link-atag" href="#projects">
+        <div className="flex items-center space-x-2">
+          <Link to="projects" smooth={true} duration={1000}>
+            <a
+              className="text-blue-600 hover:text-pink-500 hover:text-shadow-md"
+              href="#projects"
+            >
               Projects
             </a>
           </Link>
-          <span>/</span>
-          <Link className="hero-link" to="about" smooth={true} duration={1000}>
-            <a className="hero-link-atag" href="#about">
+          <span className="mx-1 text-white bg-clip-text text-shadow-md">/</span>
+          <Link to="about" smooth={true} duration={1000}>
+            <a
+              className="text-blue-600 hover:text-pink-500 hover:text-shadow-md"
+              href="#about"
+            >
               About
             </a>
           </Link>
-          <span>/</span>
-          <Link
-            className="hero-link"
-            to="connect"
-            smooth={true}
-            duration={1000}
-          >
-            <a className="hero-link-atag" href="#connect">
+          <span className="mx-1 text-white bg-clip-text text-shadow-md">/</span>
+          <Link to="connect" smooth={true} duration={1000}>
+            <a
+              className="text-blue-600 hover:text-pink-500 hover:text-shadow-md"
+              href="#connect"
+            >
               Connect
             </a>
           </Link>
         </div>
       </div>
-      <ul className="hero-icons-container">
-        <li className="hero-icons">
+      <ul className="fixed right-1 bottom-2 list-none z-10 flex flex-col items-end space-y-2">
+        <li className="mr-2">
           <a
             id="git"
             href="https://github.com/justigo86"
             target="_blank"
             rel="noreferrer noopener"
+            className="text-white hover:text-[#0473fc]"
           >
             <FaGithub />
           </a>
         </li>
-        <li className="hero-icons">
+        <li className="mr-2">
           <a
             id="mail"
             href="mailto: justigo86@gmail.com"
             target="_blank"
             rel="noreferrer noopener"
+            className="text-white hover:text-[#7b00ac]"
           >
             <FaEnvelope />
           </a>
         </li>
-        <li className="hero-icons">
+        <li className="mr-2">
           <a
             id="linked"
             href="https://www.linkedin.com/in/justin-otto-m-s-ed-s-60b449100"
             target="_blank"
             rel="noreferrer noopener"
+            className="text-white hover:text-[#bc0098]"
           >
             <FaLinkedin />
           </a>
         </li>
-        <li className="hero-icons">
+        <li className="mr-2">
           <a
             id="resume"
             href={resume.src}
             target="_blank"
             rel="noreferrer noopener"
+            className="text-white hover:text-[#ff0099]"
           >
             <FaRegFileAlt />
           </a>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,6 @@
+/* eslint-disable quotes */
 import type { Config } from "tailwindcss";
+import plugin from "tailwindcss/plugin";
 
 const config = {
   content: [
@@ -10,12 +12,50 @@ const config = {
   prefix: "",
   darkMode: ["class"], // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        // prettier-ignore
+        'hero': "url('/src/images/coffee1.jpg')",
+      },
+      spacing: {
+        "88": "22rem",
+      },
+      animation: {
+        heroLoad: "heroLoad 0.7s",
+        imageLoad: "imageLoad ease 2s",
+      },
+      keyframes: {
+        heroLoad: {
+          "0%": { left: "-8vw" },
+        },
+        imageLoad: {
+          "0%": { opacity: "0" },
+          "100%": { opacity: "1" },
+        },
+      },
+      brightness: {
+        45: ".45",
+      },
+      grayscale: {
+        40: ".40",
+      },
+      textShadow: {
+        md: "2px 2px 0 #000",
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ addUtilities }) {
+      addUtilities({
+        ".text-shadow-md": {
+          "text-shadow": "2px 2px 0 #000",
+        },
+      });
+    }),
+  ],
 } satisfies Config;
 
 export default config;
